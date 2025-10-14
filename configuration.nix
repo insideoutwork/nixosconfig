c776b9834d944305ce153a57fb785f5d30d5ff26
insideoutwork <insideoutwork@protonmail.com>
2025-10-14 21:19:15 +0200

    numlock
    code to activate numlock not working -> removed

--- configuration.nix
+++ configuration.nix
@@ -235,18 +235,6 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  systemd.services.numLockOnTty = {
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      # /run/current-system/sw/bin/setleds -D +num < "$tty";
-      ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
-        for tty in /dev/tty{1..6}; do
-            ${pkgs.kbd}/bin/setleds -D +num < "$tty";
-        done
-      '');
-    };
-  };
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
